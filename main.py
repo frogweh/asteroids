705caf0d3fa987f5548050692043a6f7ceab8588
Frogweh <zbernath@frogweh.org>
2025-01-21 11:22:13 -0700

    added game loop, drew screen, and gracefully close out when game window is closed

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ import pygame
 import constants
 
 def main():
-    print(f"Starting asteroids!\nScreen width: {constants.SCREEN_WIDTH}\nScreen height: {constants.SCREEN_HEIGHT}")    
+    screen_width = constants.SCREEN_WIDTH
+    screen_height = constants.SCREEN_HEIGHT
+    print(f"Starting asteroids!\nScreen width: {screen_width}\nScreen height: {screen_height}")
+    pygame.init()
+    screen = pygame.display.set_mode((screen_width,screen_height))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
